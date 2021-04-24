0423b3cfac536521f0610492b742dfebcacfdb8f
Calvyn <calvyn360@gmail.com>
2021-04-23 23:11:16 -0400

    "First 5 hours"

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let pants = 9;
-const pants2 = 10;
+const number = 1;
+let number2 = number;
+number2 = 7;
+console.log(`The first valuue is ${number} and the second is ${number2}`);
 
-console.log(pants+pants2);
-
-let n01 = 3;
-let n02 = '5';
-console.log(typeof '56');
+let person = { name: `Job` };
+let person2 = {...person};
+person2.name = `Susie`
+console.log(`The first is ${person.name} and the second is ${person2.name}`)
