79ebf856d228ccf9aecf1148da182c83cbdace5e
Calvyn <calvyn360@gmail.com>
2021-04-24 22:03:55 -0400

    Trying out typescript

--- script.js
+++
@@ -1,9 +0,0 @@
-const number = 1;
-let number2 = number;
-number2 = 7;
-console.log(`The first valuue is ${number} and the second is ${number2}`);
-
-let person = { name: `Job` };
-let person2 = {...person};
-person2.name = `Susie`
-console.log(`The first is ${person.name} and the second is ${person2.name}`)
---
+++ test.ts
@@ -0,0 +1,17 @@
+const months = [
+    `January`,
+    `February`,
+    `March`,
+    `April`,
+    `May`,
+    `June`
+]
+
+const date = new Date();
+const month = date.getMonth();
+const day = date.getDay();
+const dates = date.getDate();
+console.log(date.getFullYear());
+
+
+document.body.innerHTML = date;
